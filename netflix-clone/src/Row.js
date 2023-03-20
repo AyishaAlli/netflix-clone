427819cc680e73a94706e1cb07c76ebd36a7cd7f
Ayisha Alli <alli.ayisha1@gmail.com>
2023-03-20 21:59:49 +0000

    removed gitignore - error

--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -3,12 +3,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./Row.css";
+import YouTube from "react-youtube";
 
 const base_url = "https://image.tmdb.org/t/p/original";
 
 function Row({ title, fetchUrl, isLargeRow }) {
   // use state - stores info untill refreshed
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   // a snippet of code which runs based on a specific condition/variable
   // when the page loads this code runs
@@ -17,7 +19,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
       //console.log(request);
-      setMovies(request.data.results); // Array of results 
+      setMovies(request.data.results); // Array of results
       return request;
     }
     fetchData();
@@ -25,7 +27,27 @@ function Row({ title, fetchUrl, isLargeRow }) {
     // if bracket is blank, the code runs once.
     // to get async function to work, you need to include the variable thats outside the block below so it gets re-rendered
   }, [fetchUrl]);
-  // console.log(movies);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search)
+          setTrailerUrl(urlParams.get('v'))
+        })
+        .catch((err) => console.log(err));
+    }
+  };
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -33,6 +55,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row_poster ${isLargeRow && "row_poster_large"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -42,7 +65,8 @@ function Row({ title, fetchUrl, isLargeRow }) {
         ))}
       </div>
 
-      {/* container -> posters */}
+      {/* Embedded youtube vidoes */}
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
